6793a4ad0775f28836b2f96e202460cca3ef0e27
Ben Godfrey <bcgodfrey91@gmail.com>
2017-09-26 22:44:00 -0500

    shadowing

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,13 @@ fn main() {
     println!("The value of x is: {}", x);
     x = 6;
     println!("The value of x is: {}", x);
+
+    // shadowing
+    let y = 5;
+
+    let y = y + 1;
+
+    let y = y * 2;
+
+    println!("The value of x is: {}", y);
 }
